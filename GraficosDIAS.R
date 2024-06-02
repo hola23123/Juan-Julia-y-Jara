8cbfee6110b61425651e91110d7b1dc4875ef287
Juanclopp <juancalvolopez28@gmail.com>
2024-06-02 20:31:55 +0200

    cambio dias

--- GraficosDIAS.R
+++ GraficosDIAS.R
@@ -5,7 +5,7 @@ library(ggplot2)
 
 clean_data <- read_excel("ForoData.xlsx")
 
-#CREAR UNA COLUMNA CON SOLO LA FECHA
+# CREAR UNA COLUMNA CON SOLO LA FECHA
 clean_data <- clean_data %>%
   mutate(Only_Date = case_when(
     str_detect(Date, "^\\d{2}-\\w{3}-\\d{4}") ~ dmy(substr(Date, 1, 11)),
@@ -13,12 +13,21 @@ clean_data <- clean_data %>%
     TRUE ~ as.Date(NA)
   ))
 
-#CONTAR LOS MENSAJES POR DIA SEGUN LA COLUMNA Only_Date
+# CONTAR LOS MENSAJES POR DIA SEGUN LA COLUMNA Only_Date
 mensajes_por_dia <- clean_data %>%
   group_by(Only_Date) %>%
   summarise(n_mensajes = n())
 
-#GRAFICA CON GGPLOT
+# CREAR UNA SECUENCIA COMPLETA DE FECHAS INCLUYENDO EL DÍA 14 DE MAYO
+complete_dates <- data.frame(Only_Date = seq(min(mensajes_por_dia$Only_Date), as.Date("2024-05-14"), by = "day"))
+
+# UNIR LA SECUENCIA COMPLETA DE FECHAS CON LOS DATOS CONTADOS
+mensajes_por_dia <- complete_dates %>%
+  left_join(mensajes_por_dia, by = "Only_Date") %>%
+  replace_na(list(n_mensajes = 0)) %>%
+  filter(!Only_Date %in% as.Date(c("2024-05-15", "2024-05-16", "2024-05-17")))
+
+# GRAFICA CON GGPLOT
 ggplot(mensajes_por_dia, aes(x = Only_Date, y = n_mensajes)) +
   geom_bar(stat = "identity", fill = "steelblue") +
   labs(title = "Cantidad de mensajes por día",
